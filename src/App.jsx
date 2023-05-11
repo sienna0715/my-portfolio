7bc0f3cbe09ef24695838b1666ed4aee61eabd53
sienna0715 <5609373@naver.com>
2023-05-11 17:44:27 +0900

    Top 버튼 생성

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ import Header from "./components/Header";
 import Resume from "./components/Resume";
 import { useState } from "react";
 import styled from "styled-components";
+import { AiOutlineArrowUp } from "react-icons/ai";
 
 function App() {
   const [currentTab, setCurrentTab] = useState("Portfolio");
 
+  const handleClickTop = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
     <BrowserRouter>
       <BodyWrap>
+        <Top onClick={handleClickTop}>
+          <AiOutlineArrowUp />
+        </Top>
         <Header currentTab={currentTab} setCurrentTab={setCurrentTab} />
         <Routes>
           <Route
@@ -41,4 +49,24 @@ const BodyWrap = styled.div`
   width: 1000px;
   height: 100%;
   background-color: #fff;
+`;
+
+const Top = styled.div`
+  position: fixed;
+  bottom: 1.5rem;
+  right: 1.5rem;
+  background-color: #fff;
+  width: 4rem;
+  height: 4rem;
+  font-size: 2rem;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  transition: 0.7s;
+  &:hover {
+    background-color: #242323;
+    color: #fff;
+  }
 `;
